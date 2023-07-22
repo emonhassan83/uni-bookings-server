6e9e241fb600f5059fee9eac959cf0ee98d3c0de
Emon <emonhasan7650@gmail.com>
2023-07-23 00:33:37 +0600

    mongodb connection and get route add

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -12,6 +13,56 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(express.json());
+
+
+//connect to mondoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mt8kgrw.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const collegesCollection = client.db("uniBookingsDB").collection("colleges");
+
+
+    //get all colleges
+    app.get('/colleges',  async (req, res) => {
+      const colleges = await collegesCollection.find().toArray();
+      res.send(colleges);
+    })
+
+
+    //get single colleges
+    app.get('/college/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const college = await collegesCollection.findOne(query);
+      res.send(college);
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get("/", (req, res) => {
